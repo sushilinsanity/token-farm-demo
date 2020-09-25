279846e20eba906fea47472c79c9606f23fd8539
Sushil <sushilmangal731@gmail.com>
2020-09-25 16:31:35 +0530

    Updates

---
+++ precache-manifest.4007869c5e1a7626563d6c7e8722d58a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1ff9354b2648edce1990",
+    "url": "/token-farm-demo/static/css/main.071fca2e.chunk.css"
+  },
+  {
+    "revision": "1ff9354b2648edce1990",
+    "url": "/token-farm-demo/static/js/main.1ff9354b.chunk.js"
+  },
+  {
+    "revision": "2bf7044a815d2afe1729",
+    "url": "/token-farm-demo/static/css/1.5cc80121.chunk.css"
+  },
+  {
+    "revision": "2bf7044a815d2afe1729",
+    "url": "/token-farm-demo/static/js/1.2bf7044a.chunk.js"
+  },
+  {
+    "revision": "6221ea48737ac81b0c8b",
+    "url": "/token-farm-demo/static/js/runtime~main.6221ea48.js"
+  },
+  {
+    "revision": "fcd0b0325d7aabec4c09b9f91887ddd2",
+    "url": "/token-farm-demo/index.html"
+  }
+];
